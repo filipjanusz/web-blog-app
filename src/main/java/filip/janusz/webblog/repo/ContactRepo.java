cd60deb400f6e290613b44a74e4c28a549f46288
Filip Janusz <filip.janusz57@gmail.com>
2016-10-21 20:53:51 +0200

    Fix 101

--- src/main/java/filip/janusz/webblog/repo/ContactRepo.java
+++
@@ -1,9 +0,0 @@
-package filip.janusz.webblog.repo;
-
-import filip.janusz.webblog.domain.Contact;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepo extends MongoRepository<Contact, String> {
-}
